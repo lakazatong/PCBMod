0ebe0e802b8114ca906294b3ac640d1c522240df
lakazatong <Bo_wood@outlook.fr>
2025-03-05 18:05:15 +0100

    Update Main.java

--- common/src/main/java/in/lakazatong/pcbmod/redstone/parser/examples/and_gate/Main.java
+++ common/src/main/java/in/lakazatong/pcbmod/redstone/parser/examples/and_gate/Main.java
@@ -1,17 +1,18 @@
 package in.lakazatong.pcbmod.redstone.parser.examples.and_gate;
 
+import in.lakazatong.pcbmod.redstone.parser.Parser;
 import in.lakazatong.pcbmod.redstone.parser.Structure;
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Structure w = Structure.fromNBT(Path.of("common/src/main/java/in/lakazatong/pcbmod/redstone/parser/examples/and_gate/and_gate.nbt"));
-//        System.out.println(w);
-//
-//        Parser p = new Parser(w);
-//        Object G = p.parse();
-//
-//        System.out.println(G);
+        Structure structure = Structure.fromNBT(Path.of("common/src/main/java/in/lakazatong/pcbmod/redstone/parser/examples/and_gate/and_gate.nbt"));
+        System.out.println(structure);
+
+        Parser p = new Parser(structure);
+        Object G = p.parse();
+
+        System.out.println(G);
     }
 }
