a5f4ab4b513490d7cacac1a863b188b1e3d417e7
lakazatong <Bo_wood@outlook.fr>
2025-03-11 19:44:14 +0100

    fabric mod template

---
+++ src/client/java/net/lakazatong/pcbmod/PCBModClient.java
@@ -0,0 +1,10 @@
+package net.lakazatong.pcbmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PCBModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
